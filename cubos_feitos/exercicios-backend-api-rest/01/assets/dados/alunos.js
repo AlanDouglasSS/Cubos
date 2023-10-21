00f7aefb51f61b91858e2a7a42e0dc6efa4574ca
Alan Santos <46723421+AlanDouglasSS@users.noreply.github.com>
2023-10-21 20:05:19 -0300

    Delete cubos_feitos directory

--- cubos_feitos/exercicios-backend-api-rest/01/assets/dados/alunos.js
+++
@@ -1,8 +0,0 @@
-// dados/alunos.js
-// Bem-vindo aos bastidores secretos - aqui é onde guardamos os dançarinos!
-
-// Nosso palco secreto - ops, array de alunos!
-const alunos = [];
-
-// Mágica acontece aqui - o array de dançarinos cresce (ou não)!
-module.exports = alunos;
